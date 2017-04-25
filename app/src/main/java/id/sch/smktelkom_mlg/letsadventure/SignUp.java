cca7551c1f7f15747302f030a17051af97e57fb7
rehanarroihan <third2014project@gmail.com>
2017-04-26 01:28:21 +0700

    navigation menu

---
+++ app/src/main/java/id/sch/smktelkom_mlg/letsadventure/SignUp.java
@@ -0,0 +1,9 @@
+package id.sch.smktelkom_mlg.letsadventure;
+
+/**
+ * Created by Rehan on 26/04/2017.
+ */
+
+public interface SignUp {
+    //
+}
---
+++ app/src/main/java/id/sch/smktelkom_mlg/letsadventure/fragment/AccountFragment.java
@@ -0,0 +1,31 @@
+package id.sch.smktelkom_mlg.letsadventure.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import id.sch.smktelkom_mlg.letsadventure.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AccountFragment extends Fragment {
+
+
+    public AccountFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_account, container, false);
+    }
+
+}
